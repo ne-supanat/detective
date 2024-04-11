cbb79d0d9e3a70923d11ec718c51137c7830ce5c
ne-supanat <naesup@gmail.com>
2024-04-11 15:43:39 +0700

    update title & icon

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Detective Game',
+      title: 'AIdentity',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
